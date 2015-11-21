bd35f2c498df8f5791cf8160cb571385436d5a40
Adam <Adam@Adam-PC>
2015-11-21 22:16:47 +0000

    Chunking the database update for speed.

--- updateBD.php
+++ updateBD.php
@@ -17,9 +17,10 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully" . PHP_EOL;
-    
+
     //Quick query
-    $val = mysql_query('select 1 from jitamarket LIMIT 1');
+    $val = $conn->prepare('select 1 from jitamarket LIMIT 1');
+    $val->execute();
 
     //Checking if the table exists and then creating it if it doesn't
     if ($val !== FALSE) {
@@ -34,12 +35,11 @@ try {
         $sql->execute();
         echo "Table created successfully" . PHP_EOL;
     }
-    
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage() . PHP_EOL;
 }
 try {
-    
+
     //Selecting all itemID's that are buyable on the market and are not BP's or skils
     $query = $conn->prepare("SELECT typeID FROM `invtypes` WHERE invtypes.groupID NOT IN (SELECT groupID FROM `invgroups` WHERE categoryID IN (9, 16)) AND `marketGroupID` IS NOT NULL");
     $query->execute();
@@ -51,45 +51,56 @@ try {
     echo "Connection failed to invtypes: " . $e->getMessage();
 }
 
-foreach ($item_ids as $value) {
+//chunk array into smaller arrays
+
+
+$chunked_array = array_chunk($item_ids, 100, TRUE);
+
+//for each smaller array construct the url
+foreach ($chunked_array as $chunk) {
+//$chunk = $chunked_array[0];
     set_time_limit(30);
+    $item_string = '';
+    foreach ($chunk as $value) {
+        $item_string .= 'typeid=' . intval($value['typeID']) . '&';
+    }
 
-    $item_id = intval($value['typeID']);
+    echo 'Quering market data for ' . count($chunk) . ' items' . PHP_EOL;
 
-    echo 'Quering market data for item: ' . $item_id . PHP_EOL;
-
-    $curl_handle = curl_init('http://api.eve-central.com/api/marketstat?typeid=' . $item_id . '&usesystem=30000142');
+    $curl_handle = curl_init('http://api.eve-central.com/api/marketstat?' . $item_string . 'usesystem=30000142');
     curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true); // Fetch the contents too
     $http = curl_exec($curl_handle);
 
     curl_close($curl_handle);
 
-    echo 'Data returned. ' . PHP_EOL;
+    echo 'Webpage returned.' . PHP_EOL;
 
     $xml = simplexml_load_string($http);
+    echo 'Data returned ' . $xml->children()->children()->count() . PHP_EOL;
 
-    //add the data returned to the BD
-    if ($xml !== null) {
-        $buy = (string) $xml->marketstat->type->buy->max; // max buy price
-        $sell = (string) $xml->marketstat->type->sell->min;
-        $buy_vol = (string) $xml->marketstat->type->buy->volume;
+//enter returned values into DB
+    foreach ($xml->children()->children() as $item) {
 
-        try {
-            $row_exists = $conn->query("SELECT * FROM `jitamarket` WHERE typeID=" . $item_id);
-            if ($row_exists) {
-                $sql = $conn->prepare("UPDATE jitamarket SET volume='$buy_vol', max='$buy', min='$sell'");
+        $item_id = $item->attributes();
+
+        //add the data returned to the BD
+        if ($xml !== FALSE) {
+            $buy = implode($item->xpath("/buy/max"));
+            $sell = implode($item->xpath("/sell/min"));
+            $buy_vol = implode($item->xpath("/buy/volume"));
+
+            try {
+                $sql = $conn->query("INSERT INTO jitamarket (typeID, volume, max, min) "
+                        . "VALUES ('$item_id', '$buy_vol', '$buy', '$sell') ON DUPLICATE KEY "
+                        . "UPDATE volume=VALUES(volume), max=VALUES(max), min=VALUES(min)");
                 $sql->execute();
-                echo "Data updated for " . $item_id . PHP_EOL;
-            } else {
-                $sql = $conn->prepare("INSERT INTO jitamarket (typeID, volume, max, min) "
-                        . "VALUES ('$item_id', '$buy_vol', '$buy', '$sell')");
-                $sql->execute();
+
                 echo "Data entered for " . $item_id . PHP_EOL;
+                
+            } catch (PDOException $e) {
+                echo "Cannot add entry: " . $e->getMessage();
             }
-        } catch (PDOException $e) {
-            echo "Cannot add entry: " . $e->getMessage();
         }
     }
-
 }
 ?>
