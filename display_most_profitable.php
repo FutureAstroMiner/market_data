8dc4998c17403d5dd56fad0b1e60a4b4bee0c2e5
STProgrammer <adam@adam-taylor.me.uk>
2015-12-14 21:27:04 +0000

    Correct SQL statement

--- display_most_profitable.php
+++ display_most_profitable.php
@@ -4,6 +4,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * localhost/Eve_industry_data/display_most_profitable.php
  */
 $servername = "localhost";
 $username = "root";
@@ -20,7 +22,7 @@ try {
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage() . PHP_EOL;
 }
-   $query = $conn->prepare("SELECT * FROM `jitamarket` ORDER BY 'profit' LIMIT 20");
+   $query = $conn->prepare("SELECT * FROM `jitamarket` ORDER BY profit DESC Limit 0,20 ");
     $query->execute();
 
     $profitable_items = $query->fetchAll();
