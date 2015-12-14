a3ccc404b938fbcd56bbe9f63e56c2949b3eac0f
STProgrammer <adam@adam-taylor.me.uk>
2015-12-14 18:02:19 +0000

    Initial commit. file to display best items

---
+++ display_most_profitable.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully" . PHP_EOL;
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage() . PHP_EOL;
+}
+   $query = $conn->prepare("SELECT * FROM `jitamarket` ORDER BY 'profit' LIMIT 20");
+    $query->execute();
+
+    $profitable_items = $query->fetchAll();
+
+    print_r($profitable_items);
+?>
