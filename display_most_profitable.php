2b57ac6c332c536ca0ec1647373c0d7f39ac542d
Adam <Adam@Adam-PC>
2016-04-30 21:24:08 +0100

    Removed market groups 19 (trade goods) from display most profitable.
    Notes updated

--- display_most_profitable.php
+++ display_most_profitable.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -24,24 +24,24 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully" . PHP_EOL;
-
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage() . PHP_EOL;
 }
-   $query = $conn->prepare(
-           "SELECT i.typeID, i.typeName, j.typeID, j.sellVolume, j.buyVolume, j.max, j.min, j.profit, j.delta "
-           . "FROM invtypes AS i "
-           . "INNER JOIN jitamarket AS J ON j.typeID = i.typeID "
-           . "WHERE j.buyVolume > {$buyVol} AND j.sellVolume > {$sellVol} "
-           . "AND j.delta > {$delta} "
-           . "AND i.marketGroupID <> 19 " //market group 19 is trade goods so removing it
-           . "ORDER BY j.profit DESC Limit 0,30");
-    $query->execute();
+$query = $conn->prepare(
+        "SELECT i.typeID, i.typeName, j.typeID, j.sellVolume, j.buyVolume, j.max, j.min, j.profit, j.delta "
+        . "FROM invtypes AS i "
+        . "INNER JOIN jitamarket AS J ON j.typeID = i.typeID "
+        . "WHERE j.buyVolume > {$buyVol} AND j.sellVolume > {$sellVol} "
+        . "AND j.delta > {$delta} "
+        . "AND i.marketGroupID NOT IN (SELECT `marketGroupID` FROM `invmarketgroups` "
+                . "WHERE `parentGroupID` = '19' ORDER BY `marketGroupName`) " //market group 19 is trade goods so removing it
+        . "ORDER BY j.profit DESC Limit 0,30");
+$query->execute();
 
-    $profitable_items = $query->fetchAll();
-    //echo "<pre>" . print_r($profitable_items) . "</pre>";
-    
-    echo '<table>
+$profitable_items = $query->fetchAll();
+//echo "<pre>" . print_r($profitable_items) . "</pre>";
+
+echo '<table>
     <tr>
     <td>Item Name</td>
     <td>Sell Volume</td>
@@ -51,17 +51,16 @@ try {
     <td>Delta</td>
     <td>Total Potential Profit</td>
   </tr>';
-    foreach ($profitable_items as $item) {
-        echo "<tr>"
-        . "<td>{$item['typeName']}</td>"
-        . "<td>{$item['sellVolume']}</td>"
-        . "<td>{$item['buyVolume']}</td>"
-        . "<td>{$item['max']}</td>"
-        . "<td>{$item['min']}</td>"
-        . "<td>{$item['delta']}</td>"
-        . "<td>{$item['profit']}</td>"
-        . "</tr>";
-    }
-    echo "</table>";
-    
+foreach ($profitable_items as $item) {
+    echo "<tr>"
+    . "<td>{$item['typeName']}</td>"
+    . "<td>{$item['sellVolume']}</td>"
+    . "<td>{$item['buyVolume']}</td>"
+    . "<td>{$item['max']}</td>"
+    . "<td>{$item['min']}</td>"
+    . "<td>{$item['delta']}</td>"
+    . "<td>{$item['profit']}</td>"
+    . "</tr>";
+}
+echo "</table>";
 ?>
